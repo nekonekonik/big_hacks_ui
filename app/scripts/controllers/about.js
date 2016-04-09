576a84263bbed0eb199276eb80ee4dba6b14baf9
Nicholette <nekonekonik@gmail.com>
2016-04-09 04:37:04 -0700

    init view

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bigHacksUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bigHacksUiApp
+ */
+angular.module('bigHacksUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bigHacksUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bigHacksUiApp
+ */
+angular.module('bigHacksUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
