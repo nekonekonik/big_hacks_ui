d670065e7dc91ea004ff0a3b914d1d52fb7bef61
kenhua-l <l_kenhua93@hotmail.com>
2016-04-09 12:22:57 -0700

    Timeline working with mock JSON

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,56 +7,85 @@
  * # MainCtrl
  * Controller of the bigHacksUiApp
  */
+/*JSONprop - name, message, country, time*/
+
+var people = [{
+	name: 'Nadia Gomeza',
+	country: 'Russia'
+},
+{
+	name: 'Hanwan Kong',
+	country: 'China'
+},
+{
+	name: 'Yakob Dermalitzki',
+	country: 'Russia'
+},
+{
+	name: 'Gomez Castillo',
+	country: 'Mexico'
+},
+{
+	name: 'Leticia Evalon',
+	country: 'Philippines'
+},
+{
+	name: 'Shanti Arumugam',
+	country: 'India'
+},
+{
+	name: 'Meiling Yang',
+	country: 'China'
+},
+{
+	name: 'Alphonse Peran',
+	country: 'Philippines'
+},
+{
+	name: 'Tanisha Chopra',
+	country: 'India'
+},
+{
+	name: 'Eduardo Souza',
+	country: 'Mexico'
+}];
+
+var json = [{"orignalMessage":"Je  m'appelle Vinod","translatedMessage":"My name is Vinod","messageTime":"2016-04-09T12:30:23.000Z","sender":"+16506031106"},{"orignalMessage":"Serious ah bro","translatedMessage":"Serious ah bro","messageTime":"2016-04-09T09:14:11.000Z","sender":"+16506031106"},{"orignalMessage":"Hi","translatedMessage":"Hi","messageTime":"2016-04-09T09:04:20.000Z","sender":"+16506031106"}];
+
+var randomIndex = function(){
+	return Math.floor((Math.random() * 10));
+};
+
+var randomPeople = function(){
+	return people[randomIndex()];
+};
+
 angular.module('bigHacksUiApp')
   .controller('MainCtrl', function ($scope) {
-
 	  // in controller
-	  $scope.events = [{
-	    badgeClass: 'info',
-	    badgeIconClass: 'glyphicon-check',
-	    title: 'First heading',
-	    content: 'Some awesome content.'
-	  }, 
-	  {
-	    badgeClass: 'warning',
-	    badgeIconClass: 'glyphicon-credit-card',
-	    title: 'Second heading',
-	    content: 'More awesome content.'
-	  },
-	  {
-	    badgeClass: 'info',
-	    badgeIconClass: 'glyphicon-check',
-	    title: 'First heading',
-	    content: 'Some awesome content.'
-	  }, 
-	  {
-	    badgeClass: 'warning',
-	    badgeIconClass: 'glyphicon-credit-card',
-	    title: 'Second heading',
-	    content: 'More awesome content.'
-	  },
-	  {
-	    badgeClass: 'info',
-	    badgeIconClass: 'glyphicon-check',
-	    title: 'First heading',
-	    content: 'Some awesome content.'
-	  }, 
-	  {
-	    badgeClass: 'warning',
-	    badgeIconClass: 'glyphicon-credit-card',
-	    title: 'Second heading',
-	    content: 'More awesome content.'
-	  }];
-
-	// optional: not mandatory (uses angular-scroll-animate)
-	$scope.animateElementIn = function($el) {
-		$el.removeClass('timeline-hidden');
-		$el.addClass('bounce-in');
-	};
-
-	// optional: not mandatory (uses angular-scroll-animate)
-	$scope.animateElementOut = function($el) {
-		$el.addClass('timeline-hidden');
-		$el.removeClass('bounce-in');
-	};
+	$scope.events = [
+	];
+	
+	json.forEach(function(entry){
+		var contact = randomPeople();
+		contact.message = entry.translatedMessage;
+		contact.messageTime = entry.messageTime;
+		$scope.events.push(contact);
+	});
+	// $http({
+ //  		method: 'GET',
+ //  		url: 'http://big-hacks.herokuapp.com/messagelist'
+	// }).then(function successCallback(response) {
+ //    	// this callback will be called asynchronously
+ //    	// when the response is available
+ //    	console.log(response[0]);
+ //    	var contact = randomPeople();
+ //    	contact.message = response[0].translatedMessage;
+ //    	contact.messageTime = response[0].messageTime;
+ //    	$scope.events.push(contact);
+ //    	console.log(contact);
+ //  	});//, //function errorCallback(response) {
+    	// called asynchronously if an error occurs
+    	// or server returns response with an error status.
+  	//});
   });
