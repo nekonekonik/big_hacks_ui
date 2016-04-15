becb26123137b1be55640847386f1228dd533da1
Nicholette Li <nekonekonik@gmail.com>
2016-04-15 10:37:37 -0700

    Update main.js
    Code cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,56 +1,8 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name bigHacksUiApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the bigHacksUiApp
- */
-/*JSONprop - name, message, country, time, star,*/
-
-
-// var profilesArr = [{
-// 	'orignalMessage': 'Je  m\'appelle Vinod',
-// 	'translatedMessage': 'My business has been blooming ever since I got funded by donors from KIVA to buy my shop. I couldn\'t have done it without their support',
-// 	'messageTime': '2016-04-09T12:30:23.000Z',
-// 	'sender': '+16506031106',
-// 	'star' : '3'
-// }, {
-// 	'orignalMessage': 'Serious ah bro',
-// 	'translatedMessage': 'Thank you for the opportunity given by KIVA\'s donors. The financial opportunity has given me the power to give back to my society.',
-// 	'messageTime': '2016-04-09T09:14:11.000Z',
-// 	'sender': '+16506031106',
-// 	'star' :'2.5'
-// }, {
-// 	'orignalMessage': 'Hi',
-// 	'translatedMessage': 'I bought 20 hens for my small egg farm with the money provided. My children now learns to rear them for me too.',
-// 	'messageTime': '2016-04-09T09:04:20.000Z',
-// 	'sender': '+16506031106',
-// 	'star': '4'
-// }, {
-// 	'orignalMessage': 'Voulez vous couchez avec moi',
-// 	'translatedMessage': 'It was a tough time for me because the typhoon destroyed the crops. Thanks to the money I received, I managed to build a decent business today.',
-// 	'messageTime': '2016-04-09T12:32:45.000Z',
-// 	'sender': '+16506031106',
-// 	'star':'3.5'
-// },{
-// 	'orignalMessage': 'Sweet',
-// 	'translatedMessage': 'The money has provided me with a great relief after I lost everything to the natural disaster.',
-// 	'messageTime': '2016-04-09T10:33:42.000Z',
-// 	'sender': '+16506031106',
-// 	'star': '3'
-// }, {
-// 	'orignalMessage': 'Bonjour',
-// 	'translatedMessage': 'I have opened a school with the money to help my village\'s children get education which can help them tremendously in their future.',
-// 	'messageTime': '2016-04-09T15:20:29.000Z',
-// 	'sender': '+16506031106',
-// 	'star': '4.5'
-// }];
-
-
 angular.module('bigHacksUiApp')
 .controller('MainCtrl', function ($scope, $http, poller) {
+    var NGROK_URL = '';
 
 	  // in controller
 	  $scope.events = [
@@ -132,7 +84,7 @@ angular.module('bigHacksUiApp')
 		}
 	};
 
-	resultPoller = poller.get('http://a183674a.ngrok.io/message', {
+	resultPoller = poller.get(NGROK_URL + '/message', {
                           delay: 1500     // delay for 5 seconds
                       });
 	resultPoller.promise.then(null, null, pollResults);
@@ -147,7 +99,7 @@ angular.module('bigHacksUiApp')
 	function initialize() {
 		$http({
 			method: 'GET',
-			url: 'http://a183674a.ngrok.io/messagelist'
+			url: NGROK_URL + '/messagelist'
 		}).then(function successCallback(response) {
 		     	// this callback will be called asynchronously
 		     	// when the response is available
